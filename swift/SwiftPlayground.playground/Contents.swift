c528b66156dc76a32b2aef2561e8c437a30115b2
mrvink <vinkdesign@gmail.com>
2016-06-20 16:47:26 +0200

    #9 Parametres

--- swift/SwiftPlayground.playground/Contents.swift
+++ swift/SwiftPlayground.playground/Contents.swift
@@ -100,6 +100,22 @@ class Person {
 }
 
 
+// 29 PARAMETRES
+
+class Counter {
+    var count: Int = 0
+    func incrementBy(amount: Int, numberOfTimes times: Int) {
+        count += amount * times
+    }
+}
+
+var counter = Counter()
+counter.incrementBy(2, numberOfTimes: 7)
+
+
+
+
+
 
 
 
